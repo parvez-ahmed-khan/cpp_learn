e2a154f1a3869d1b8149fbd953ff484b3702f689
parvezkhan11bd <engparvezkhan.pk@gmail.com>
2025-12-09 07:21:07 +0600

    new file added

---
+++ variable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5, m=3;
+    int sum=n+m;
+    int sub=n-m;
+    cout<<"Sum is:"<<sum<<endl;
+    cout<<"Sub is:"<<sub<<endl;
+    
+    return 0;
+
+}
